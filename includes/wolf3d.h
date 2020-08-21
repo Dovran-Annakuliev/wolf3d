fd7610a63ffb0b46ded1f3921f205d2c112485fc
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-08-21 17:39:20 +0300

    mg

---
+++ includes/wolf3d.h
@@ -0,0 +1,18 @@
+#ifndef WOLF3D_WOLF3D_H
+#define WOLF3D_WOLF3D_H
+
+#ifdef __APPLE__
+# include <OpenGL/gl.h>
+# include <OpenCL/opencl.h>
+#else
+# include <GL/opengl.h>
+# include <CL/opencl.h>
+#endif
+
+#include "../libft/includes/libft.h"
+#include "../SDL2.framework/Headers/SDL.h"
+#include "../SDL2_image.framework/Headers/SDL_image.h"
+
+#include <stdio.h>
+
+#endif
---
+++ sources/main.c
@@ -0,0 +1,6 @@
+#include <std>
+
+int main()
+{
+
+}
