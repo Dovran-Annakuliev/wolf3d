85d58272b5c29790fdc41d5a77d087dcec65d178
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-08-28 16:04:37 +0300

    read_arg fin

--- sources/read_arg.c
+++ sources/read_arg.c
@@ -13,7 +13,25 @@ static int	ft_strlen_split(char const *s, char c)
 	return (len);
 }
 
-static void	split(t_db *data, char *line, int i)
+static void	set_status(t_db *data, int k, char *status)
+{
+	if (ft_strcmp(status, "1") == 0)
+		data->map.map[k].status = '1';
+	else if (ft_strcmp(status, "A") == 0)
+		data->map.map[k].status = 'A';
+	else if (ft_strcmp(status, "E") == 0)
+		data->map.map[k].status = 'E';
+	else if (ft_strcmp(status, "P") == 0)
+		data->map.map[k].status = 'P';
+	else if (ft_strcmp(status, "W") == 0)
+		data->map.map[k].status = 'W';
+	else if (ft_strcmp(status, "M") == 0)
+		data->map.map[k].status = 'M';
+	else
+		data->map.map[k].status = '0';
+}
+
+static void	ft_split(t_db *data, char *line, int i)
 {
 	int		k;
 	int		j;
@@ -24,20 +42,7 @@ static void	split(t_db *data, char *line, int i)
 	array = ft_strsplit(line, ' ');
 	while (array[++j])
 	{
-		if (ft_strcmp(array[j], "1") == 0)
-			data->map.map[k].status = '1';
-		else if (ft_strcmp(array[j], "A") == 0)
-			data->map.map[k].status = 'A';
-		else if (ft_strcmp(array[j], "E") == 0)
-			data->map.map[k].status = 'E';
-		else if (ft_strcmp(array[j], "P") == 0)
-			data->map.map[k].status = 'P';
-		else if (ft_strcmp(array[j], "W") == 0)
-			data->map.map[k].status = 'W';
-		else if (ft_strcmp(array[j], "M") == 0)
-			data->map.map[k].status = 'M';
-		else
-			data->map.map[k].status = '0';
+		set_status(data, i, array[j]);
 		data->map.map[k].x = j;
 		data->map.map[k].y = i;
 		free(array[j]);
@@ -59,7 +64,7 @@ static void	read_size(int fd, char *line, t_db *data)
 	}
 }
 
-void read_arg(char *source, t_db *data)
+void		read_arg(char *source, t_db *data)
 {
 	char	*line;
 	int		i;
@@ -73,15 +78,25 @@ void read_arg(char *source, t_db *data)
 		error(1);
 	read_size(fd, line, data);
 	data->map.map = (t_point *)malloc(sizeof(t_point)
-								  * data->map.heg * data->map.len);
+			* data->map.heg * data->map.len);
 	close(fd);
 	i = -1;
 	fd = open(source, O_RDONLY);
 	while (get_next_line(fd, &line))
 	{
-		split(data, line, ++i);
+		ft_split(data, line, ++i);
 		ft_strdel(&line);
 	}
 	free(line);
 	close(fd);
 }
+
+/*
+** 0 - Empty
+** 1 - Wall
+** A - Ammo
+** E - Enemy
+** P - Player
+** W - Weapon
+** M - Medicine
+*/
