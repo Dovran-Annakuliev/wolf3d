44efb9259e726064357b7b45b3367674e1e040c0
ltammie <55656332+ltammie@users.noreply.github.com>
2020-10-04 17:48:08 +0300

    g

--- sources/player.c
+++ sources/player.c
@@ -1,48 +1,4 @@
 #include "../includes/wolf3d.h"
-//
-//void draw_fov(t_db *data, float angle, int i)
-//{
-//	float view_line;
-//	float x;
-//	float y;
-//	int k;
-//
-//	view_line = 0;
-//	while (view_line <= 20)
-//	{
-//		x = data->player.x + view_line * cosf(angle);
-//		y = data->player.y + view_line * sinf(angle);
-//		k = (int)x + (int)y * data->map.len;
-//		if (data->map.cell[k].wall != 0)
-//			break;
-//		view_line += 0.05f;
-//	}
-//	SDL_SetRenderDrawColor(data->sdl.renderer, 0, 0, 255, 255);
-//	SDL_RenderDrawLineF(data->sdl.renderer, data->player.x * data->map.fill,
-//					 data->player.y * data->map.fill, x * data->map.fill, y * data->map.fill);
-//	SDL_SetRenderDrawColor(data->sdl.renderer, 255, 0, 0, 255);
-//
-//	unsigned line2 = data->sdl.height / (view_line * cosf(angle - data->player.pov * (float)M_PI / 180)) / 2;
-//	SDL_RenderDrawLineF(data->sdl.renderer, i, data->sdl.height / 2 - line2, i, data->sdl.height / 2 + line2);
-//}
-//
-//void player_present(t_db *data)
-//{
-//	float angle;
-//	float step;
-//	float fin;
-//
-//	angle = (data->player.pov - data->player.fov / 2) * (float)M_PI / 180;
-//	step = (data->player.fov * (float)M_PI / 180) / data->sdl.width;
-//	fin = (data->player.pov + data->player.fov / 2) * (float)M_PI / 180;
-//
-//	int i = 0;
-//	while (angle <= fin)
-//	{
-//		draw_fov(data, angle, i++);
-//		angle += step;
-//	}
-//}
 
 static int		check_wall(int x, int y, t_db *data, float angle)
 {
@@ -52,7 +8,6 @@ static int		check_wall(int x, int y, t_db *data, float angle)
 		return (0);
 	return (1);
 }
-
 
 //devision by zero! if angle == 0 or 180 degress
 static float	horizontal_intersect(int i, t_db *data, float angle)
